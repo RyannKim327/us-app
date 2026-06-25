0de7f25751e1823b4af3cc5db12a88d390630a0f
RyannKim327 <rksesgundo123@gmail.com>
2026-06-26 07:36:13 +0800

    06-26-26  7:36 [setups]

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  devtools: { enabled: false },
+  modules: ['@nuxtjs/tailwindcss'],
+  routeRules: {
+    // Generated at build time for SEO purpose
+    '/': { prerender: true },
+    // Cached for 1 hour
+    '/api/*': { cache: { maxAge: 60 * 60 } },
+    // Redirection to avoid 404
+    '/old-page': {
+      redirect: { to: '/new-page', statusCode: 302 },
+    },
+    // ...
+  },
 })
