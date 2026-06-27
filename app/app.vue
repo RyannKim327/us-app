ba09f10643cf46b2a11d36850c5858985cabeaf3
RyannKim327 <rksesgundo123@gmail.com>
2026-06-27 08:23:52 +0800

    06-27-26  8:23 [setups]

--- app/app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="h-dvh w-dvw text-white bg-slate-950 relative overflow-hidden font-sans select-none">
-    <!-- Ambient Background Lights -->
-    <div class="absolute -top-40 -left-40 h-[600px] w-[600px] rounded-full bg-blue-600/10 blur-[120px] pointer-events-none"></div>
-    <div class="absolute -bottom-40 -right-40 h-[600px] w-[600px] rounded-full bg-cyan-500/10 blur-[120px] pointer-events-none"></div>
-    
-    <NuxtPage class="relative z-10" />
-  </div>
-</template>
